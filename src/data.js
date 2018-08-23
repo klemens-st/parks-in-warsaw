553dae6e8b66ee31618d467374bbb35d528d971a
Klemens Starybrat <klemens.starybrat@gmail.com>
2018-08-23 20:14:17 +0200

    fix: Remove a park with no foursquare matches

--- src/data.js
+++ src/data.js
@@ -4,10 +4,9 @@ const parks = [
   {id: 2, title: 'Skaryszewski Park', location: {lat: 52.242052, lng: 21.054391}},
   {id: 3, title: 'Mokotow Field', location: {lat: 52.2106505, lng: 20.9984688}},
   {id: 4, title: 'Multimedia Fountain Park', location: {lat: 52.2542102, lng: 21.0112982}},
-  {id: 5, title: 'Dolina Służewiecka Park', location: {lat: 52.16816859999999, lng: 21.0329227}},
-  {id: 6, title: 'Ogród Krasińskich', location: {lat: 52.2484032, lng: 21.0025514}},
-  {id: 7, title: 'Praski Park', location: {lat: 52.2528201, lng: 21.027713}},
-  {id: 8, title: 'Moczydło Park', location: {lat: 52.240961, lng: 20.953227}},
+  {id: 5, title: 'Ogród Krasińskich', location: {lat: 52.2484032, lng: 21.0025514}},
+  {id: 6, title: 'Praski Park', location: {lat: 52.2528201, lng: 21.027713}},
+  {id: 7, title: 'Moczydło Park', location: {lat: 52.240961, lng: 20.953227}},
 ];
 
 export default parks;
